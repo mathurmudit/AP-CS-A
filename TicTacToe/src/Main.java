14f302d807800062ecb8fe6182ca8f9e4e377a71
Mudit Mathur <mathurmudit03@gmail.com>
2020-08-05 16:29:01 -0400

    Update and rename main.java to Main.java
    Filled in main method.

---
+++ TicTacToe/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+	    //Create an instance of the TicTacToeGame Class here and 
+	    //write the appropriate program using the methods from 
+	    //the TicTacToeGame class to play a game
+	    
+	    TicTacToeGame game1 = new TicTacToeGame();
+	    
+	    while(!(game1.checkforWinner())){
+	      game1.playTurn();
+	      game1.printBoard();
+	    }
+	  }
+}
